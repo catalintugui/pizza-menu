4c4c67581133d0a2206e9d5173a0b68bc117c406
Catalin Tugui <katamaxx23@gmail.com>
2023-07-05 00:27:40 +0300

    working on index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Something</h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
