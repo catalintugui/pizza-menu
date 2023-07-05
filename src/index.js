b7c64ca0b4508410f11d1c63702761dd4f3afa85
Catalin Tugui <katamaxx23@gmail.com>
2023-07-06 00:04:44 +0300

    added few styling

--- src/index.js
+++ src/index.js
@@ -4,10 +4,36 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Something</h1>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Fast React Pizza & Co.</h1>;
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <Pizza />
+    </main>
+  );
+}
+
+function Pizza() {
+  return <h2>pizza</h2>;
+}
+
+function Footer() {
+  return <h2>footer</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
